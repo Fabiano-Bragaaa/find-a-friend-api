12fee3fe261437c112d29b7512b43419c646e2b0
Fabiano Braga <fabianobragadev@gmail.com>
2025-06-23 16:52:12 -0300

    feat: get user profile use case

---
+++ src/use-cases/get-user-profile.ts
@@ -0,0 +1,26 @@
+import { usersRepository } from '@/repositories/users-repository'
+import { User } from 'generated/prisma'
+
+interface GetUserProfileUseCaseRequest {
+  userId: string
+}
+
+interface GetUserProfileUseCaseResponse {
+  user: User
+}
+
+export class GetUserProfileUseCase {
+  constructor(private userRepository: usersRepository) {}
+
+  async execute({
+    userId,
+  }: GetUserProfileUseCaseRequest): Promise<GetUserProfileUseCaseResponse> {
+    const user = await this.userRepository.findById(userId)
+
+    if (!user) {
+      throw new Error()
+    }
+
+    return { user }
+  }
+}
