31f444d021f982b2b7c9d19776866a307074fdf5
Fabiano Braga <fabianobragadev@gmail.com>
2025-07-07 18:16:46 -0300

    feat: create test envrionment

--- src/http/controllers/pets/routes.ts
+++ src/http/controllers/pets/routes.ts
@@ -1,6 +1,13 @@
 import { FastifyInstance } from 'fastify'
 import { create } from './create'
+import { VerifyJwt } from '@/http/middlewares/verify-jwt'
 
 export async function petsRoutes(app: FastifyInstance) {
-  app.post('/pets', create)
+  app.post(
+    '/pets',
+    {
+      onRequest: [VerifyJwt],
+    },
+    create,
+  )
 }
